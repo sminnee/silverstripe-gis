bda1cbcf8e9e285fcf80f7c74ad37f2669738b3f
Ingo Schommer <ingo@silverstripe.com>
2009-09-10 04:08:16 +0000

    MINOR Updated master language tables

--- lang/en_US.php
+++ lang/en_US.php
@@ -6,5 +6,15 @@ $lang['en_US']['GeoPointField']['X'] = 'Longitude';
 $lang['en_US']['GeoPointField']['Y'] = 'Latitude';
 $lang['en_US']['GeocoderField']['LOCATIONNOTFOUND'] = 'Location can\'t be found';
 $lang['en_US']['GeocoderField']['LOCATIONNOTUNIQUE'] = 'Location is not unique, please be more specific';
+$lang['en_US']['TileRenderQueue']['PLURALNAME'] = array(
+	'Tile Render Queus',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['TileRenderQueue']['SINGULARNAME'] = array(
+	'Tile Render Queue',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
 
 ?>
